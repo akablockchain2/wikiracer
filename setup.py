fe897256995d8d03b6a533e24e2b4d354cf6bc07
akablockchain2 <akablockchain2@gmail.com>
2024-11-05 09:19:14 +0300

    fix

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 setup(
     name='wikiracer',
     version='0.1',
-    packages=find_packages(),
     include_package_data=True,
+    packages=find_packages(),
     install_requires=[
-        'requests',
-        'beautifulsoup4',
-        'click'
+        "requests",
+        "beautifulsoup4",
+        "click",
     ],
     entry_points={
         'console_scripts': [
-            'wikiracer = wikiracer.cli:main',
+            'wikiracer=wikiracer.cli:main',
         ],
     },
     description='CLI tool to extract and display links from a Wikipedia page.',
